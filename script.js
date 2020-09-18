8c4aac59fc7ec7f0817aa9b04f01417571881ec0
John Garner <jdextergarneriv@gmail.com>
2020-09-18 13:54:10 -0400

    Completed passwordOptions Function. Includes prompt and confirm questions, set variables based off questions, and establishes possible choices through if/else statements.

--- script.js
+++ script.js
@@ -3,11 +3,55 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+        var charSet = {
+            lowerCase: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+            upperCase: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+            numbers: [0,1,2,3,4,5,6,7,8,9],
+            special: ['!','"','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','?','@','^','_','`','|','~']
+        }
 
+    
+
+        //function that wraps all the prompts --> will be generate password
+
+        function passwordOptions() {
+            // Prompt asks user password length
+            var charLength = prompt ("How many characters would you like your password to be?")
+            
+            // If password length is less than 8 return to home
+            if (charLength < 8) {
+            return alert("Password length must be more than 8 characters");}
+                
+            // If password length is 8 or greater, ask if user would like to use upperCase, useNumbers, or useSpecial.
+            else if (charLength >= 8) {
+            var useUpperCase = confirm ("Do you want to use upper case letters?");
+            var useNumbers = confirm ("Do you want to use numbers?");
+            var useSpecial = confirm ("Do you want to use special characters?");
+            } 
+
+            // If - Else if and Else statements regarding selection of password characteristics 
+            if (useUpperCase === false && useNumbers === false && useSpecial === false){
+                return console.log("lower case only");
+            } else if (useUpperCase === true && useNumbers === false  && useSpecial === false) {
+                return console.log("lower case and upper case only");
+            } else if (useUpperCase === true && useNumbers === true && useSpecial === false) {
+                return console.log("lower case, upper case and numbers only");
+            } else if (useUpperCase === true && useNumbers === false && useSpecial === true) {
+                return console.log("lower case, upper case and special only");
+            } else if (useUpperCase === false && useNumbers === true && useSpecial === false){
+                return console.log("lower case and numbers only");
+            } else if (useUpperCase === false && useNumbers === true && useSpecial === true){
+                return console.log("lower case, numbers and special only");
+            } else if (useUpperCase === false && useNumbers === false && useSpecial === true){
+                return console.log("lower case and special only");
+            } else {
+                return console.log("all charSet");
+            }
+
+
+        }
+        passwordOptions();
 }
 
 // Add event listener to generate button
